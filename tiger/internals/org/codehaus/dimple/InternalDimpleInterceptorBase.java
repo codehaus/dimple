d42ebacc0f73d5a2b087eac693721df75c1a17c2
benyu <benyu@cb32ef49-7a24-0410-a53b-fd971edb0e16>
2007-07-05 21:23:16 +0000

    asm integrated

---
+++ tiger/internals/org/codehaus/dimple/InternalDimpleInterceptorBase.java
@@ -0,0 +1,13 @@
+package org.codehaus.dimple;
+
+/**
+ * All generated interceptors extend from this class.
+ * This class is public just so the generated classes
+ * can use it. 
+ * It is internal use only.
+ * @author benyu
+ */
+public abstract class InternalDimpleInterceptorBase {
+  protected Object interceptee;
+  protected Object overrider;
+}
